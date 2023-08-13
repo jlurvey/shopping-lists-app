3ddff6fc475f7946e7ceb070c4f75aff39d2fb24
Joseph Lurvey <joeylurvey@gmail.com>
2023-08-13 11:41:11 -0400

    Integrate items data fetching in useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    fetch('http://localhost:3000/items')
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
