a85aa25d297f8b00d7ca6e791f90c87bf001ed60
Joseph Lurvey <joeylurvey@gmail.com>
2023-08-18 19:48:33 -0400

    Move form from Items to it's own component

---
+++ src/components/ItemForm.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+function ItemForm({ onAddItem, }) {
+
+    //contolled form state
+    const [form, setForm] = useState({
+        name: '',
+        store: '',
+    });
+
+    //controlled form listener
+    function handleChange(e) {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    //submitForm event listener, POST new item to server with need=true, update items state
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(`Name: ${form.name}, Store: ${form.store}`)
+        const formData = {
+            name: form.name,
+            need: true,
+            store: form.store,
+        };
+        fetch('http://localhost:3000/items', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData),
+        })
+            .then((r) => r.json())
+            .then((newItem) => onAddItem(newItem));
+        //reset form
+        setForm({
+            name: '',
+            store: ''
+        });
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                Item Name: <input
+                    type='text'
+                    name='name'
+                    value={form.name}
+                    onChange={handleChange}
+                />
+                Store: <input
+                    type='text'
+                    name='store'
+                    value={form.store}
+                    onChange={handleChange}
+                />
+                <button type='submit'>Add Item</button>
+            </form>
+        </div>
+
+    )
+
+};
+
+export default ItemForm;
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,46 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import Item from "./Item";
+import ItemForm from './ItemForm';
 
 function Items({ items, onAddItem }) {
-
-    //contolled form state
-    const [form, setForm] = useState({
-        name: '',
-        store: '',
-    });
-
-    //controlled form listener
-    function handleChange(e) {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
-        });
-    };
-
-    //submitForm event listener, POST new item to server with need=true, update items state
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(`Name: ${form.name}, Store: ${form.store}`)
-        const formData = {
-            name: form.name,
-            need: true,
-            store: form.store,
-        };
-        fetch('http://localhost:3000/items', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(formData),
-        })
-            .then((r) => r.json())
-            .then((newItem) => onAddItem(newItem));
-        //reset form
-        setForm({
-            name: '',
-            store: ''
-        });
-    };
-
 
     return (
         <div>
@@ -52,21 +14,7 @@ function Items({ items, onAddItem }) {
                     need={item.need} //remove need from items list, only make available on Lists
                 />
             ))}
-            <form onSubmit={handleSubmit}>
-                Item Name: <input
-                    type='text'
-                    name='name'
-                    value={form.name}
-                    onChange={handleChange}
-                />
-                Store: <input
-                    type='text'
-                    name='store'
-                    value={form.store}
-                    onChange={handleChange}
-                />
-                <button type='submit'>Add Item</button>
-            </form>
+            <ItemForm onAddItem={onAddItem}/>
         </div>
     );
 }
