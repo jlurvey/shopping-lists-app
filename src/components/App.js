eba7312fb14ac5e8826ee63d13009dd60c7315f7
Joseph Lurvey <joeylurvey@gmail.com>
2023-08-20 12:31:50 -0400

    Initially sort alphabetically then by need

--- src/components/App.js
+++ src/components/App.js
@@ -9,115 +9,131 @@ import { Route, Switch } from 'react-router-dom'
 
 function App() {
 
-  //items, stores useState
-  const [items, setItems] = useState([]);
-  const [stores, setStores] = useState([])
-  const [displayedStoreName, setDisplayedStoreName] = useState('');
+    //items, stores useState
+    const [items, setItems] = useState([]);
+    const [stores, setStores] = useState([])
+    const [displayedStoreName, setDisplayedStoreName] = useState('');
 
-  //fetch items data
-  useEffect(() => {
-    fetch('http://localhost:3000/items')
-      .then((r) => r.json())
-      .then((data) => {
-        setItems(data);
-      });
-  }, []);
+    //fetch items data
+    useEffect(() => {
+        fetch('http://localhost:3000/items')
+            .then((r) => r.json())
+            .then((data) => {
+                setItems(
+                    data
+                        .sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase()))
+                        .sort((a, b) => b.need - a.need)
+                )
+            });
+    }, []);
 
-  //fetch stores data
-  useEffect(() => {
-    fetch('http://localhost:3000/stores')
-      .then((r) => r.json())
-      .then((data) => {
-        setStores(data);
-      });
-  }, []);
+    //fetch stores data
+    useEffect(() => {
+        fetch('http://localhost:3000/stores')
+            .then((r) => r.json())
+            .then((data) => {
+                setStores(data.sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase())));
+            });
+    }, []);
 
-  //addItem update items state
-  function handleAddItem(newItem) {
-    setItems([...items, newItem])
-  };
+    //addItem update items state
+    function handleAddItem(newItem) {
+        const updatedItems = [...items, newItem]
+        //alphabetically sort
+        updatedItems.sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase()))
+        //trueFirst sort
+        updatedItems.sort((a, b) => b.need - a.need)
+        setItems(updatedItems)
+    };
 
-  //updateNeed update items state
-  function handleUpdateNeed(updatedItem) {
-    //map to create new array with updated item
-    const updatedItems = items.map((item) => {
-      if (item.id === updatedItem.id) {
-        return updatedItem;
-      } else {
-        return item
-      }
-    });
-    setItems(updatedItems);
-  };
+    //updateNeed update items state
+    function handleUpdateNeed(updatedItem) {
+        //map to create new array with updated item
+        const updatedItems = items.map((item) => {
+            if (item.id === updatedItem.id) {
+                return updatedItem;
+            } else {
+                return item
+            }
+        });
+        //alphabetically sort
+        updatedItems.sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase()))
+        //trueFirst sort
+        updatedItems.sort((a, b) => b.need - a.need)
+        setItems(updatedItems)
+    };
 
-  //addStore update stores state
-  function handleAddStore(newStore) {
-    setStores([...stores, newStore])
-  };
+    //addStore update stores state
+    function handleAddStore(newStore) {
+        const updatedStores = [...stores, newStore]
+        //alphabetically sort
+        updatedStores.sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase()))
+        setStores(updatedStores)
+    };
 
-  //fetch DELETE items
-  function handleDeleteItemsById(id) {
-    //DELETE request
-    fetch(`http://localhost:3000/items/${id}`, {
-      method: 'DELETE',
-    })
-      .then((r) => r.json())
-      .then(() => {
-        //const updatedItems = items.filter((item) => item.id !== id);
-        const updatedItems = (prevItems => prevItems.filter((item) => item.id !== id))
-        setItems(updatedItems);
-      });
-  };
+    //fetch DELETE items
+    function handleDeleteItemsById(id) {
+        //DELETE request
+        fetch(`http://localhost:3000/items/${id}`, {
+            method: 'DELETE',
+        })
+            .then((r) => r.json())
+            .then(() => {
+                //const updatedItems = items.filter((item) => item.id !== id);
+                const updatedItems = (prevItems => prevItems.filter((item) => item.id !== id))
+                setItems(updatedItems);
+            });
+    };
 
-  //deleteStore update stores state
-  function handleDeleteStore(id, name) {
-    const updatedStores = stores.filter((store) => store.id !== id);
-    const deletedItems = items.filter((item) => item.store === name)
-    setStores(updatedStores);
-    deletedItems.forEach((item) => {
-      handleDeleteItemsById(item.id)
-    });
-  };
+    //deleteStore update stores state
+    function handleDeleteStore(id, name) {
+        const updatedStores = stores.filter((store) => store.id !== id);
+        const deletedItems = items.filter((item) => item.store === name)
+        setStores(updatedStores);
+        deletedItems.forEach((item) => {
+            handleDeleteItemsById(item.id)
+        });
+    };
 
-  return (
-    <div className="App">
-      <header >
-        Shopping Lists
-      </header>
-      <NavBar />
-      <Switch>
-        <Route exact path='/lists'>
-          <Lists
-            items={items}
-            onAddItem={handleAddItem}
-            onDeleteItem={handleDeleteItemsById}
-            onUpdateNeed={handleUpdateNeed}
-            stores={stores}
-            displayedStoreName={displayedStoreName}
-            setDisplayedStoreName={setDisplayedStoreName}
-          />
-        </Route>
-        <Route exact path='/items'>
-          <Items
-            items={items}
-            onAddItem={handleAddItem}
-            onDeleteItem={handleDeleteItemsById}
-            onUpdateNeed={handleUpdateNeed}
-            stores={stores}
-            displayedStoreName={displayedStoreName}
-            setDisplayedStoreName={setDisplayedStoreName}
-          />
-        </Route>
-        <Route exact path='/stores'>
-          <Stores
-            stores={stores}
-            onAddStore={handleAddStore}
-            onDeleteStore={handleDeleteStore}
-          />
-        </Route>
-      </Switch>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header >
+                Shopping Lists
+            </header>
+            <NavBar />
+            <Switch>
+                <Route exact path='/lists'>
+                    <Lists
+                        items={items}
+                        onAddItem={handleAddItem}
+                        onDeleteItem={handleDeleteItemsById}
+                        onUpdateNeed={handleUpdateNeed}
+                        stores={stores}
+                        displayedStoreName={displayedStoreName}
+                        setDisplayedStoreName={setDisplayedStoreName}
+                    />
+                </Route>
+                <Route exact path='/items'>
+                    <Items
+                        items={items}
+                        onAddItem={handleAddItem}
+                        onDeleteItem={handleDeleteItemsById}
+                        onUpdateNeed={handleUpdateNeed}
+                        stores={stores}
+                        displayedStoreName={displayedStoreName}
+                        setDisplayedStoreName={setDisplayedStoreName}
+                    />
+                </Route>
+                <Route exact path='/stores'>
+                    <Stores
+                        stores={stores}
+                        onAddStore={handleAddStore}
+                        onDeleteStore={handleDeleteStore}
+                    />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
