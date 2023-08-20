0521e472507888aef7f057d346245b0b28fc7beb
Joseph Lurvey <joeylurvey@gmail.com>
2023-08-19 21:51:33 -0400

    Add state for items to display

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -2,6 +2,9 @@ import React, {useState} from "react";
 
 function Lists({ items, onAddItem, onDeleteItem, onUpdateNeed, stores }) {
 
+    const [itemsToDisplay,setItemsToDisplay] = useState(items.filter((item)=>item.store===stores[0].name));
+    console.log(itemsToDisplay);
+
     return (
         <div className='App'>
             {stores.map((store) => (
